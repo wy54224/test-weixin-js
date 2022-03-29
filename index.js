09005a84bd8e5f8b4c21f862c83858534bb38c40
wy54224 <wy54224@gmail.com>
2022-03-29 15:30:21 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const request = require('request')
 const express = require('express')
 const app = express()
+const dns = require('dns')
 
 app.get('/', (req, res) => {
   const api = `https://api.weixin.qq.com/cgi-bin/get_api_domain_ip`
+  dns.lookup('www.qq.com', function(err, address, family){
+    console.log('dns err: ' + err);
+    console.log('address: ' + address);
+    console.log('family: ' + family);
+  });
   request(api, {
     method: 'GET'
   }, (err, resp, body) => {
+    console.log('request err: ' + err);
+    console.log('response: ' + resp);
     try {
       res.send(body);
     } catch (error) {
